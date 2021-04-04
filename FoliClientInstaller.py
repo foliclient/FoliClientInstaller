15425076eac1dfaa4da2a70c2c1ff2027deca3bb
eboy 3 <lolhaxx@protonmail.com>
2021-04-04 14:52:06 -0400

    Small changes, added icon

--- FoliClientInstaller.py
+++ FoliClientInstaller.py
@@ -1,43 +1,65 @@
+# Foli Client Installer v1.0.0 for Minecraft 1.16.4/5
+# Script by ablazingeboy#7375
+# Other credits in README.md
+
+# Imports
 import sys
 import os
+from os import path
 import shutil
 
-if len(sys.argv) > 2:
-    print('[ERROR]\tUh oh, there seems to be too many command arguments! If you\'ve only put in your minecraft directory as an argument, try wrapping it in quotes!')
-    print('\nPress any key to exit...')
-    input()
-    sys.exit()
+# ASCII Title
+print('\n\'||\'\'\'\'|        \'||`      .|\'\'\'\', \'||`                        ||    \n ||  .           ||   \'\'  ||       ||   \'\'                    ||    \n ||\'\'|   .|\'\'|,  ||   ||  ||       ||   ||  .|\'\'|, `||\'\'|,  \'\'||\'\'  \n ||      ||  ||  ||   ||  ||       ||   ||  ||..||  ||  ||    ||    \n.||.     `|..|\' .||. .||. `|....\' .||. .||. `|...  .||  ||.   `|..\' \n')
+# Remember to change version flag and mc version when updating!
+print('Installing Foli Client v1.0.0 for Minecraft 1.16.4/5')
+print('Installer made with <3 by ablazingeboy#7375')
+print('Learn more or submit any issues at https://github.com/ablazingeboy/FoliClientInstaller\n')
+print('While unlikely, this program has the chance of screwing up your system if used incorrectly.\nI AM NOT RESPONSIBLE FOR ANY DATA LOSS INCURRED BY USING THIS SCRIPT.\nFor best results, use this on a fresh minecraft profile, and Fabric MUST be installed.\n')
 
+# Sets or asks for what path to install to
 destPath = ''
-
 if len(sys.argv) == 2:
     destpath = sys.argv[1]
 else:
     print('[INPUT]\tPlease type in the full filepath of your .minecraft folder:')
     destpath = input()
 
+# Validates that the installation path is valid
 isdir = os.path.isdir(destpath)
-
 if not isdir:
-    print('[ERROR]\tUh oh, that\'s not a folder! Please check for any typos!')
-    print('\nPress any key to exit...')
+    print(f'[ERROR]\tUh oh, \"{destpath}\" is not a folder on your computer! Please check for any typos!')
+    print('\n[INPUT]\tPress any key to exit...')
     input()
     sys.exit()
 else:
     print('[LOG]\t' + destpath + ' is a valid directory')
 
-sourcepath = os.path.dirname(__file__) + '\\resources'
+    # Prompts user to confirm install directory
+    confirmedDir = False
+    while confirmedDir == False:
+        print(f'\n[INPUT]\tAre you sure you want to install to \"{destpath}\"? (Y\\N)')
+        choice = input().lower()
+        if choice == 'y':
+            confirmedDir = True
+        elif choice == 'n':
+            print('\n[INPUT]\tPress any key to exit...')
+            input()
+            sys.exit()
+        else:
+            print('[ERROR]\tNot a valid choice, please try again!')
 
+# Makes sure the resources folder exists
+sourcepath = path.abspath(path.join(path.dirname(__file__), 'resources'))
 if not os.path.isdir(sourcepath):
-    print('[ERROR]\tCan\'t find the resources folder. Please re-download the package or make sure the resources folder is in the same directory as the python script')
-    print('\nPress any key to exit...')
+    print('[ERROR]\tCan\'t find the resources folder. Please re-download the package or make sure the resources folder is in the same folder as this program')
+    print('\n[INPUT]\tPress any key to exit...')
     input()
     sys.exit()
 else:
     print('[LOG]\tValidated resource directory')
 
+# Walks the resource directory and copies files
 print('\n')
-
 for subdir, dirs, files in os.walk(sourcepath):
     for filename in files:
         filepath = subdir + os.sep + filename
@@ -45,9 +67,10 @@ for subdir, dirs, files in os.walk(sourcepath):
         os.makedirs(destpath + destination, exist_ok=True)
         fulldestpath = destpath + destination + os.sep + filename
         shutil.copyfile(filepath, fulldestpath)
-        print(f'[LOG]\tCopied {filepath} to {fulldestpath}')
+        print(f'[LOG]\tCopied {destination} to {fulldestpath}')
 
-print('\nAll files should have been copied over. Thanks for using Foli Client!')
-print('\nPress any key to exit...')
+# Success Message
+print('\n[LOG]\tAll files have been copied over, and Foli Client should now be installed. Remember to turn on the resourcepack!')
+print('\n[INPUT]\tPress any key to exit...')
 input()
 sys.exit()
